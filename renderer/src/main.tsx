2aa346407f707b3203ac85dc60ed27dca42fe574
Mark Doyle <39464373+doylemark@users.noreply.github.com>
2022-02-21 18:35:59 +0000

    use react 18

--- renderer/src/main.tsx
+++ renderer/src/main.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
+import "./index.css";
+import App from "./App";
+
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("React failed to mount, couldn't find container");
+}
+
+const root = ReactDOM.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
