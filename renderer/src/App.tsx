522df85a5ffb795a8e817b3bb7a94f433c5c0737
Mark Doyle <39464373+doylemark@users.noreply.github.com>
2022-02-22 02:22:40 +0000

    add header

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,12 +1,15 @@
 import { Suspense } from "react";
 
 import Bookings from "./components/bookings";
+import Header from "./components/header";
 
 const App = () => {
   return (
-    <Suspense fallback={<p>loading...</p>}>
-      <Bookings url="https://hub.ucd.ie/usis/W_HU_MENU.P_PUBLISH?p_tag=GYMBOOK" />
-    </Suspense>
+    <div>
+      <Suspense fallback={<p>loading...</p>}>
+        <Bookings url="https://hub.ucd.ie/usis/W_HU_MENU.P_PUBLISH?p_tag=GYMBOOK" />
+      </Suspense>
+    </div>
   );
 };
 
--- renderer/src/components/bookings.tsx
+++ renderer/src/components/bookings.tsx
@@ -1,3 +1,6 @@
+import { LocationMarkerIcon } from "@heroicons/react/outline";
+import clsx from "clsx";
+import { useState } from "react";
 import { suspend } from "suspend-react";
 
 import {
@@ -6,6 +9,8 @@ import {
   getToken,
   Session,
 } from "../../lib/gym-sessions";
+
+import Header from "./header";
 
 type GymData = {
   sessions: Session[];
@@ -32,18 +37,48 @@ type BookingsProps = { url: string };
 
 const Bookings = ({ url }: BookingsProps) => {
   const data = suspend(() => gymDataFetcher(url), ["bookingState"]);
+  const [activeDay, setActiveDay] = useState(0);
 
   return (
-    <ol className="flex flex-col gap-6 p-6">
-      {data.sessions.map((session) => (
-        <li className="flex flex-col p-4 border shadow rounded">
-          <p className="text-lg">
-            {session.time} @ {session.location}
-          </p>
-          {session.duration}
-        </li>
-      ))}
-    </ol>
+    <>
+      <Header
+        days={data.days}
+        activeDayIdx={activeDay}
+        setActiveDay={setActiveDay}
+      />
+      <ol className="flex flex-col gap-6 p-6">
+        {data.sessions.map((session) => (
+          <li
+            key={session.id}
+            className="flex justify-between items-center p-4 border shadow rounded-lg"
+          >
+            <div>
+              <p className="text-lg flex items-center gap-1">
+                <span className="text-xl">
+                  {session.location.includes("Poolside") ? "🏖" : ""}
+                </span>
+                {session.location}{" "}
+              </p>
+              <p className="text-gray-800">
+                {new Intl.DateTimeFormat("en-us", {
+                  hour: "numeric",
+                  minute: "numeric",
+                  hour12: false,
+                }).format(new Date(data.days[activeDay] + "-" + session.time))}
+              </p>
+            </div>
+            <button
+              className={clsx(
+                "bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-md text-white focus:outline-none",
+                "focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 focus:ring-offset-white"
+              )}
+            >
+              Schedule for Booking
+            </button>
+          </li>
+        ))}
+      </ol>
+    </>
   );
 };
 
